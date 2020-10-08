acee1cf4dad55bf75a0b0529118c03e22a3d2d1c
Christopher Arsenault <christopher.arsenault82@gmail.com>
2020-10-08 15:58:11 -0400

    Pizza and toppings pages have been templated.

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,44 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
+import ToppingsFilter from '../components/ToppingsFilter';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data, pageContext }) {
+  const pizzas = data.pizzas.nodes;
   return (
-    <div>
-      <p>This is the pizzas page</p>
-    </div>
+    <>
+      <ToppingsFilter activeTopping={pageContext.topping} />
+      <PizzaList pizzas={pizzas} />
+    </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery($toppingRegex: String) {
+    pizzas: allSanityPizza(
+      filter: { toppings: { elemMatch: { name: { regex: $toppingRegex } } } }
+    ) {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ gatsby/src/templates/Pizza.js
@@ -0,0 +1,50 @@
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import React from 'react';
+import styled from 'styled-components';
+
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
+`;
+
+export default function SinglePizzaPage({ data: { pizza } }) {
+  return (
+    <PizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+        <h3>Price - ${pizza.price / 100}</h3>
+      </div>
+    </PizzaGrid>
+  );
+}
+
+// This needs to be dynamic based on the slug passed in via context on gatsby-node
+export const query = graphql`
+  query($slug: String!) {
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+      name
+      id
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        name
+        id
+        vegetarian
+      }
+      price
+    }
+  }
+`;
